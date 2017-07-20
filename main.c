d15912433068f0e5820cae0c79f8b99f36ea5779
Ed Mandy <ed@7400circuits.com>
2017-07-20 07:39:20 -0600

    Added full-screen HDMI to Freeplay LCD

--- main.c
+++ main.c
@@ -52,6 +52,26 @@ static inline void rotate90_16bpp(uint16_t *src, register uint16_t *dst, int src
     }
 }
 
+static inline void copy_16bpp_offset(uint16_t *src, register uint16_t *dst, int src_width, int src_height, int dst_offset_x, int dst_offset_y, int dst_width, int dst_height)
+{
+    int src_x, src_y;
+    uint16_t *dst_ptr;
+    
+    
+    
+    for(src_y=0; src_y<src_height; src_y++)
+    {
+        dst_ptr = dst + (dst_width * dst_offset_y) + (dst_width * src_y) + dst_offset_x;
+        
+        /*for(src_x=0; src_x<src_width; src_x++)
+         {
+         *(dst_ptr + src_x) = *(src+(src_y*src_width) + src_x);
+         }*/
+        memcpy(dst_ptr,src+(src_y*src_width),src_width*2);
+    }
+}
+
+
 int process() {
     DISPMANX_DISPLAY_HANDLE_T display;
     DISPMANX_MODEINFO_T display_info;
@@ -65,6 +85,13 @@ int process() {
     uint8_t portrait_mode_pri = 0;
     uint8_t portrait_mode_sec = 0;
     uint8_t rotate_screen = 0;
+    
+    uint8_t overscan_screen = 1;
+    int scaled_w = 16*19;//16
+    int scaled_h = 208;//16*13;
+    int offset_x = 16;//14;
+    int offset_y = 6;
+    
     
     struct fb_var_screeninfo vinfo;
     struct fb_fix_screeninfo finfo;
@@ -83,7 +110,7 @@ int process() {
         return -1;
     }
     syslog(LOG_INFO, "Primary display is %d x %d", display_info.width, display_info.height);
-    //printf("Primary display is %d x %d\n", display_info.width, display_info.height);
+    printf("Primary display is %d x %d\n", display_info.width, display_info.height);
     
     
     fbfd = open("/dev/fb1", O_RDWR);
@@ -101,7 +128,7 @@ int process() {
     }
     
     syslog(LOG_INFO, "Second display is %d x %d %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel);
-    //printf("Second display is %d x %d %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel);
+    printf("Second display is %d x %d %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel);
     
     //if vinfo.xres < vinfo.yres, then we are in portrait mode
     if(vinfo.xres < vinfo.yres)
@@ -120,6 +147,16 @@ int process() {
     if(rotate_screen)
     {
         screen_resource = vc_dispmanx_resource_create(VC_IMAGE_RGB565, vinfo.yres, vinfo.xres, &image_prt);
+        if (!screen_resource) {
+            syslog(LOG_ERR, "Unable to create screen buffer");
+            close(fbfd);
+            vc_dispmanx_display_close(display);
+            return -1;
+        }
+    }
+    else if(overscan_screen)
+    {
+        screen_resource = vc_dispmanx_resource_create(VC_IMAGE_RGB565, scaled_w, scaled_h, &image_prt);
         if (!screen_resource) {
             syslog(LOG_ERR, "Unable to create screen buffer");
             close(fbfd);
@@ -178,6 +215,54 @@ int process() {
              rotate90_16bpp_interlace((uint16_t *)image_orig, (uint16_t *)fbp, vinfo.yres, vinfo.xres, 1);
              }
              interlace = !interlace;*/
+            
+            usleep(20 * 1000);//sleep N microseconds
+        }
+    }
+    else if(overscan_screen)
+    {
+        /*
+         #overscan_left=60
+         #overscan_right=0
+         #overscan_top=26
+         #overscan_bottom=80
+         
+         
+         
+         
+         
+         320 - 60 		= 260
+         240 - 26 - 80 	= 134
+         */
+        
+        
+        vc_dispmanx_rect_set(&rect1, 0, 0, scaled_w, scaled_h);
+        
+        void *image_scaled = calloc( 1, scaled_w * scaled_h * vinfo.bits_per_pixel / 8 );
+        
+        if(!image_scaled)
+            return -1;
+        
+        while (1) {
+            char *fbp_offset;
+            
+            //snapshot the display
+            ret = vc_dispmanx_snapshot(display, screen_resource, 0);
+            
+            //read data from snapshot into image_orig
+            vc_dispmanx_resource_read_data(screen_resource, &rect1, image_scaled, scaled_w * vinfo.bits_per_pixel / 8);
+            //           vc_dispmanx_resource_read_data(screen_resource, &rect1, fbp, scaled_w * vinfo.bits_per_pixel / 8);
+            
+            //copy the data (straight to the fbp)
+            
+            //copy_16bpp_offset(uint16_t *src, register uint16_t *dst, int src_width, int src_height, int dst_offset_x, int dst_offset_y, int dst_width, int dst_height)
+            //copy_16bpp_offset((uint16_t *)image_scaled, (uint16_t *)fbp, scaled_w, scaled_h, offset_x, offset_y, vinfo.xres, vinfo.yres);
+            copy_16bpp_offset((uint16_t *)image_scaled, (uint16_t *)fbp, scaled_w, scaled_h, offset_x, offset_y, vinfo.xres, vinfo.yres);
+            
+            
+            //fbp_offset = fbp + (offset_y * vinfo.xres * 2) + (offset_x * 2);
+            //vc_dispmanx_resource_read_data(screen_resource, &rect1, fbp_offset, vinfo.xres * vinfo.bits_per_pixel / 8);
+            
             
             usleep(20 * 1000);//sleep N microseconds
         }
